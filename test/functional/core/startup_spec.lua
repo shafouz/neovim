9e4fd3fcf1ff5bc8be718c41831484a2a7e113a2
shafouz <38974831+shafouz@users.noreply.github.com>
2023-07-24 21:38:17 -0400

    Update test/functional/core/startup_spec.lua
    Co-authored-by: zeertzjq <zeertzjq@outlook.com>

--- test/functional/core/startup_spec.lua
+++ test/functional/core/startup_spec.lua
@@ -45,7 +45,7 @@ describe('startup', function()
     local screen
     screen = Screen.new(60, 3)
     screen:attach()
-    command('terminal ' .. nvim_prog .. ' -u NONE --server $NVIM --remote-ui')
+    funcs.termopen({ nvim_prog, '-u', 'NONE', '--server', eval('v:servername'), '--remote-ui' })
     screen:expect([[
       ^Nvim child UI cannot attach to the UI of its parent ($NVIM) |
                                                                   |
